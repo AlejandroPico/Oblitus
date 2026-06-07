26cb370565678884a8c7b1f83cf730390ea7e7ba
Alejandro Pico <115283749+AlejandroPico@users.noreply.github.com>
2026-06-07 16:40:55 +0200

    Conectar menu movil en paginas internas

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,7 +1,9 @@
 import { initTemporalTheme } from './theme.js';
+import { initMobileMenu } from './mobile-menu.js';
 
 const themeToggle = document.querySelector('#themeToggle');
 const currentYear = document.querySelector('#currentYear');
 
 initTemporalTheme(themeToggle);
+initMobileMenu();
 if (currentYear) currentYear.textContent = new Date().getFullYear();
